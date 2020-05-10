098690a262f3842b2a93757de1232a98c265cb94
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-10 16:11:43 -0300

    Initial Header component.

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const Header = () => {
+  return <Text>Header</Text>;
+};
+
+export default Header;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,15 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../screens/Home';
 import Cart from '../screens/Cart';
 
+import Header from '../components/Header';
+
 const Stack = createStackNavigator();
 
 export default function routes() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Cart" component={Cart} />
+    <Stack.Navigator screenOptions={{headerTitle: <Header />}}>
+      <Stack.Screen name="Home" component={Home} title="Home" />
+      <Stack.Screen name="Cart" component={Cart} title="Cart" />
     </Stack.Navigator>
   );
 }
