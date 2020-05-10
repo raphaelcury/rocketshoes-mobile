a475e3a0069521abe751b2d1d372e0387fb2330c
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-10 14:02:59 -0300

    Going to cart through button.

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
 // import { Container } from './styles';
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
     <View>
       <Text>Home</Text>
+      <Button title="Cart" onPress={() => navigation.navigate('cart')} />
     </View>
   );
 };
