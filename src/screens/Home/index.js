c199f5d2bdc25e19279ec859e5c06f99d2aad5c1
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-16 15:16:08 -0300

    Home product list with add buttons.

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,20 +1,56 @@
-import React from 'react';
-import {Text, Button} from 'react-native';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import {Container} from './styles';
+import api from '../../services/api';
 
-const Home = ({navigation}) => {
-  return (
-    <Container>
-      <Text style={{color: '#fff'}}>Home</Text>
-      <Button title="Cart" onPress={() => navigation.navigate('Cart')} />
-    </Container>
-  );
-};
+import {
+  Container,
+  ProductList,
+  Product,
+  ProductImage,
+  ProductDescription,
+  ProductPrice,
+  AddButton,
+  ButtonText,
+} from './styles';
 
-Home.propTypes = {
-  navigation: PropTypes.shape({navigate: PropTypes.func.isRequired}).isRequired,
-};
+export default class Home extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({navigate: PropTypes.func.isRequired})
+      .isRequired,
+  };
 
-export default Home;
+  state = {
+    productList: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('/products');
+    console.tron.log(response.data);
+    this.setState({productList: [...response.data]});
+  }
+
+  render() {
+    const {productList} = this.state;
+    console.tron.log(productList);
+    return (
+      <Container>
+        <ProductList
+          horizontal
+          data={productList}
+          keyExtrator={(product) => product.id}
+          renderItem={(product) => (
+            <Product>
+              <ProductImage source={product.image} width="60" />
+              <ProductDescription>{product.title}</ProductDescription>
+              <ProductPrice>{product.price}</ProductPrice>
+              <AddButton>
+                <ButtonText>Adicionar</ButtonText>
+              </AddButton>
+            </Product>
+          )}
+        />
+      </Container>
+    );
+  }
+}
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,7 +1,38 @@
 import styled from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
   background: #141419;
+`;
+
+export const ProductList = styled.FlatList``;
+export const Product = styled.View`
+  justify-content: center;
+  align-items: center;
+  width: 220px;
+  height: 358px;
+  background: #fff;
+  margin: 0px 10px;
+`;
+export const ProductImage = styled.Image``;
+export const ProductDescription = styled.Text`
+  color: #fff;
+`;
+export const ProductPrice = styled.Text`
+  color: #fff;
+`;
+export const AddButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #8a2be2;
+  width: 200px;
+  height: 40px;
+  border-radius: 5px;
+`;
+export const ButtonText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  text-transform: uppercase;
 `;
