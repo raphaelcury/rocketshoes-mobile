d5ed0c1fe62e31c8043b5caaa83fb7c528c14819
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-18 19:13:13 -0300

    @cart/ADD action handling.

--- src/store/modules/Cart/reducer.js
+++ src/store/modules/Cart/reducer.js
@@ -1,3 +1,13 @@
-export default function CartReducer() {
-  return [];
+import produce from 'immer';
+
+export default function CartReducer(state = [], action) {
+  console.tron.log(action.product);
+  switch (action.type) {
+    case '@cart/ADD':
+      return produce(state, (draft) => {
+        draft.push(action.product);
+      });
+    default:
+      return state;
+  }
 }
