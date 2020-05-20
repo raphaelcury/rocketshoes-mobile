48abcbf831471f750c6f37d5161ba438441ac8b3
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-20 20:30:35 -0300

    Removing console.lot from CartReducer.

--- src/store/modules/Cart/reducer.js
+++ src/store/modules/Cart/reducer.js
@@ -1,7 +1,6 @@
 import produce from 'immer';
 
 export default function CartReducer(state = [], action) {
-  console.tron.log(action.product);
   switch (action.type) {
     case '@cart/ADD':
       return produce(state, (draft) => {
