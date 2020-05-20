869cffa576017c4c9be4c552f493fb8b2ecbe83b
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-20 20:29:27 -0300

    Removing warning from rootNavigation.

--- src/routes/rootNavigation.js
+++ src/routes/rootNavigation.js
@@ -3,5 +3,7 @@ import * as React from 'react';
 export const navigationRef = React.createRef();
 
 export function navigate(route, params) {
-  navigationRef.current?.navigate(route, params);
+  if (navigationRef.current) {
+    navigationRef.current.navigate(route, params);
+  }
 }
